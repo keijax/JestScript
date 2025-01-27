f5bec212b016386124d74f38765c0156e3b2fc9c
keijax <kiahamini14@gmail.com>
2025-01-27 15:11:09 +0330

    DrumKit was added

---
+++ DrumKit/Drum Kit Starting Files/index.js
@@ -0,0 +1,58 @@
+// Add event listeners to all buttons
+var buttons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttons; i++) {
+  document
+    .querySelectorAll(".drum")
+    [i].addEventListener("click", function handleClick() {
+      var buttonInner = this.innerHTML;
+      makeSound(buttonInner);
+    });
+}
+
+//Keyboard pressing detection
+document.addEventListener("keypress", function(event)
+{
+    makeSound(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom1 = new Audio("sounds/tom-2.mp3");
+      tom1.play();
+      break;
+
+    case "s":
+      var tom1 = new Audio("sounds/tom-3.mp3");
+      tom1.play();
+      break;
+
+    case "d":
+      var tom1 = new Audio("sounds/tom-4.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      var tom1 = new Audio("sounds/snare.mp3");
+      tom1.play();
+      break;
+
+    case "k":
+      var tom1 = new Audio("sounds/kick-bass.mp3");
+      tom1.play();
+      break;
+
+    case "l":
+      var tom1 = new Audio("sounds/crash.mp3");
+      tom1.play();
+      break;
+
+    default:
+      break;
+  }
+}
